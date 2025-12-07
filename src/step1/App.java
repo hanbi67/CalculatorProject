d972d89c68ca60932ef947463752733239b45697
Lee Hanbi <hanbi6717@naver.com>
2025-12-08 04:38:21 +0900

    add positive integer input

--- src/step1/App.java
+++ src/step1/App.java
@@ -1,7 +1,39 @@
 package step1;
 
+import java.util.Scanner;
+
+//STEP 1.  클래스 없이 기본적인 연산을 수행할 수 있는 계산기 만들기
 public class App {
     public static void main(String[] args) {
-        System.out.println("프로젝트 시작 전 정상 업로드 확인하기");
+
+        Scanner scanner = new Scanner(System.in);
+
+        //양의 정수 2개(0포함) 입력 받기 - 음수는 입력받을 수 없도록 제한
+        Integer num1;
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            num1 = scanner.nextInt();
+            if (num1 < 0) {
+                System.out.println("0을 포함한 양의 정수만 입력 가능합니다. 다시 입력해주세요.");
+            } else {
+                break;
+            }
+        }
+
+        Integer num2;
+        while (true) {
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            num2 = scanner.nextInt();
+            if (num2 < 0) {
+                System.out.println("0을 포함한 양의 정수만 입력 가능합니다. 다시 입력해주세요.");
+            } else {
+                break;
+            }
+        }
+
+        System.out.println("num1: " + num1);
+        System.out.println("num2: " + num2);
+
+
     }
 }
