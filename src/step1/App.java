35eca674dbb2578cfa77c1d4837d68e889836efa
Lee Hanbi <hanbi6717@naver.com>
2025-12-08 03:56:48 +0900

    git add test

--- src/step1/App.java
+++ src/step1/App.java
@@ -2,6 +2,6 @@ package step1;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("프로젝트 시작 전 정상 업로드 확인");
+        System.out.println("프로젝트 시작 전 정상 업로드 확인하기");
     }
 }
